9606d79592e28167bfcc8046766fd83775970dac
Ben <benhuy2010@yahoo.com>
2024-09-13 17:33:51 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<nav>
+    <h1> NEDTELLING TIL JUL </h1>
+</nav>
+
+<slot />
+
+<footer>
+    <span> Laget med <span>{"<3"} </span> </span>
+</footer>
+
+<style>
+    :global(body) {
+        background-color: #333;
+        margin: 0;
+        font-family: Arial, sans-serif;
+        color: white;
+    }
+
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+    text-align: center;
+    z-index: 1000;
+  }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+    text-align: center;
+    z-index: 1000;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let christmasDate = new Date('2024-12-24T00:00:00').getTime();
+    let differenceBetweenDate = christmasDate - new Date().getTime();
+    let days: number;
+    let hours: number;
+    let minutes: number;
+    let seconds: number;
+    let interval: number;
+    
+    onMount(() => {
+        const updateTime = () => {
+            differenceBetweenDate = christmasDate - new Date().getTime();
+        }
+        interval = setInterval(() => { updateTime() }, 1000);
+
+        return () =>  {
+            clearInterval(interval);
+        }
+    })
+
+   $: days = Math.floor(differenceBetweenDate / (1000 * 60 * 60 * 24));
+   $: hours = Math.floor((differenceBetweenDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   $: minutes = Math.floor((differenceBetweenDate % (1000 * 60 * 60)) / (1000 * 60));
+   $: seconds = Math.floor((differenceBetweenDate % (1000 * 60)) / 1000);
+
+</script>
+
+<main>
+<p>
+    {days} dager og {hours} timer : {minutes} minutter : {seconds} sekunder
+</p>
+<p> Det er {days} dager til 🌲🎅🏻 </p>
+</main>
+
+<style>
+    main {
+    margin-top: 100px;
+    margin-bottom: 70px;
+    padding: 1rem;
+    max-width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+</style>
